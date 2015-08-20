d7c84d1a2d6aa6d158e6bc34e5007daf2d0d1dc1
Kelsey Brubaker <brubaker.kelsey@gmail.com>
2015-08-19 23:44:04 -0700

    adding ajax for images

--- js/main-ajax.js
+++ js/main-ajax.js
@@ -13,15 +13,18 @@ $.ajax({
   method: 'GET',
   headers: {
     'Authorization' : 'Client-ID 7e9fbbc095aa5a9'
-  },
-})
-.done(function(response){
-  photoArray = response.data.images;
-  console.log(photoArray);
-  showFromImgur();
-  for( var i = 0; i < photoArray.length; i++){
-    photoArray[i].votes = photoArray[i]+1;
   }
+})
+.done(function(res){
+  photoArray = res.data.images;
+  console.log(photoArray);
+
+  // for( var i = 0; i < photoArray.length; i++){
+  //   photoArray[i].filePath = photoArray[i].link;
+  // }
+  showFromImgur();
+
+
 })
 .fail(function(err){
   console.log(err);
@@ -41,9 +44,9 @@ function showFromImgur(){
 
   while(random1 === random2){
     random2 = Math.floor((Math.random()* photoArray.length)+1);
-
+  }
     //chart.js
-    this.Pie = function(data, options){
+    Pie = function(data, options){
       chart.Pie.defaults = {
           segmentShowStroke : true,
           segmentStrokeColor : "#fff",
@@ -61,11 +64,11 @@ function showFromImgur(){
     };
     var data = [
       {
-        // value : photoArray[random1].votes,
+        value : 1,
         color : "#00A388",
       },
       {
-        // value : photoArray[random2].votes,
+        value : 1,
         color : "#BEEB9F" ,
       },
 
@@ -74,14 +77,27 @@ function showFromImgur(){
     var canvas = document.getElementById("catChart");
     var catChart = canvas.getContext("2d");
     new Chart(catChart).Pie(data);
+
+
+    var startBrawl = new Tracker();
+    startBrawl.displayPhotos();
+
+    var leftKitty = document.getElementById('leftPhotoDiv');
+    leftKitty.addEventListener('click', startBrawl.displayWinnerLeft);
+
+    var rightKitty = document.getElementById('rightPhotoDiv');
+    rightKitty.addEventListener('click', startBrawl.displayWinnerRight);
+
+    document.getElementById('nextButton').addEventListener('click', startBrawl.displayPhotos);
+    document.getElementById('nextButton').style.cssText = "visibility: hidden;"
   };
 
-};
 
-$('anotherPic').click(function(){
-  showFromImgur();
-  console.log('hello');
-});
+
+// $('anotherPic').click(function(){
+//   showFromImgur();
+//   console.log('hello');
+// });
 
 
 
@@ -97,9 +113,6 @@ var Tracker = function(){
   this.rightPhoto;
 };
 
-//array of cat pictures
-
-// console.log(photoArray);
 
 Tracker.prototype.displayWinnerLeft = function(){
   $('#leftPhoto').css({'border': '5px solid #BEEB9F'});
@@ -118,20 +131,6 @@ Tracker.prototype.displayWinnerRight = function(){
   console.log("This cat has " + photoArray[randomInt2].votes + " votes.");
 };
 
-
-Tracker.prototype.displayPhotos = function(){
-
-  // console.log(random1);
-  // console.log(random2);
-
-  // while(randomInt1 === randomInt2){
-  //   randomInt2 = Math.floor((Math.random()* photoArray.length)+1);
-  // };
-
-  // var leftPhoto = document.getElementById("leftPhoto");
-  // var rightPhoto = document.getElementById("rightPhoto");
-  // document.getElementById("rightPhoto").src = photoArray[randomInt1].filePath;
-  // document.getElementById("leftPhoto").src = photoArray[randomInt2].filePath;
 
   //chart.js
   this.Pie = function(data, options){
@@ -152,11 +151,11 @@ Tracker.prototype.displayPhotos = function(){
   };
   var data = [
     {
-      // value : photoArray[random1].votes,
+      value : 1,
       color : "#00A388",
     },
     {
-      // value : photoArray[random2].votes,
+      value : 1,
       color : "#BEEB9F" ,
     },
 
@@ -166,33 +165,5 @@ Tracker.prototype.displayPhotos = function(){
   var catChart = canvas.getContext("2d");
   new Chart(catChart).Pie(data);
 
-}
+});//end document ready
 
-var startBrawl = new Tracker();
-startBrawl.displayPhotos();
-
-var leftKitty = document.getElementById('leftPhotoDiv');
-leftKitty.addEventListener('click', startBrawl.displayWinnerLeft);
-
-var rightKitty = document.getElementById('rightPhotoDiv');
-rightKitty.addEventListener('click', startBrawl.displayWinnerRight);
-
-document.getElementById('nextButton').addEventListener('click', startBrawl.displayPhotos);
-document.getElementById('nextButton').style.cssText = "visibility: hidden;"
-
- });//end document ready
-
-
- // photoArray.push(new Photo('bob',  'img/kittens/0.jpg',  1));
- // photoArray.push(new Photo('jim',  'img/kittens/1.jpg',  1));
- // photoArray.push(new Photo('fred',  'img/kittens/2.jpg',  1));
- // photoArray.push(new Photo('cat 4',  'img/kittens/4.jpg',  1));
- // photoArray.push(new Photo('cat 5',  'img/kittens/5.jpg',  1));
- // photoArray.push(new Photo('cat 6',  'img/kittens/6.jpg',  1));
- // photoArray.push(new Photo('cat 7',  'img/kittens/7.jpg',  1));
- // photoArray.push(new Photo('cat 8',  'img/kittens/8.jpg',  1));
- // photoArray.push(new Photo('cat 9',  'img/kittens/9.jpg',  1));
- // photoArray.push(new Photo('cat 10', 'img/kittens/10.jpg', 1));
- // photoArray.push(new Photo('cat 11', 'img/kittens/11.jpg', 1));
- // photoArray.push(new Photo('cat 12', 'img/kittens/12.jpg', 1));
- // photoArray.push(new Photo('cat 13', 'img/kittens/13.jpg', 1));
